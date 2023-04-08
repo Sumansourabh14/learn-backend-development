28f46f325d0c0b8a51bae2f48beb680ada2bb13d
Suman Sourabh <suman.sourabh@ansrsource.com>
2023-04-09 00:37:20 +0530

    added cookie parser and basic authentication

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cookieParser = require("cookie-parser");
 
 const app = express();
 
@@ -10,9 +11,23 @@ app.set("view engine", "ejs");
 
 // for accessing the form data
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
-  res.render("index", { clubName: "Chelsea", clubName2: "Liverpool" });
+  const { token } = req.cookies;
+  console.log({ token });
+
+  if (token) {
+    console.log("Token is present!");
+    res.render("logout");
+  } else {
+    console.log("Token is NOT present!");
+    res.render("login");
+  }
+});
+
+app.get("/login", (req, res) => {
+  res.render("login", { clubName: "Chelsea", clubName2: "Liverpool" });
 });
 
 app.post("/submit-form", (req, res) => {
@@ -23,9 +38,22 @@ app.post("/submit-form", (req, res) => {
   if (req.body === undefined) {
     res.send("Something wrong with submitting the form");
   } else {
+    res.cookie("token", "User logged in!", {
+      httpOnly: true,
+    });
+
     users.push({ emailOfUser: email, passwordOfUser: password });
-    res.send("Form submitted!");
+    res.redirect("/");
   }
+});
+
+app.get("/logout", (req, res) => {
+  res.cookie("token", null, {
+    httpOnly: true,
+    expires: new Date(Date.now()),
+  });
+
+  res.redirect("/login");
 });
 
 app.get("/users", (req, res) => {
