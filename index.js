baf1ab81f7b71079b07867fb63308dac3e7448e5
Suman Sourabh <suman.sourabh@ansrsource.com>
2023-04-08 13:54:41 +0530

    Added files

--- index.js
+++ index.js
@@ -1,40 +1,13 @@
-// import http from "http";
-// // import { randomVariable } from "./random.js"; // .js extension is mandatory!
-// import fs from "fs";
-// import * as randomVariables from "./random.js";
-
-// const { randomVariable, randomVariable1 } = randomVariables; // destructured!
-
-// const server = http.createServer((req, res) => {
-//   if (req.url === "/") {
-//     fs.readFile("./package.json", (err, data) => {
-//       if (err) {
-//         res.end("Error has occured: " + err.message);
-//       } else {
-//         res.end("Read successful");
-//       }
-//     });
-//   } else if (req.url === "/about") {
-//     res.end("About Page");
-//   } else {
-//     res.end("Server running");
-//   }
-// });
-
-// const port = 7000;
-
-// server.listen(port, () => {
-//   console.log("Server running at http://localhost:" + port);
-// });
-
 import express from "express";
 
 const app = express();
 
 const port = 7000;
 
+app.set("view engine", "ejs");
+
 app.get("/", (req, res) => {
-  res.send("Working!");
+  res.render("index", { clubName: "Chelsea", clubName2: "Liverpool" });
 });
 
 app.listen(port, () => {
