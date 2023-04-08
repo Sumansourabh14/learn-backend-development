ddf6cdb4d6a6933b269425cc5ae2e586705c3134
Suman Sourabh <suman.sourabh@ansrsource.com>
2023-04-08 14:44:41 +0530

    added form and post request

--- index.js
+++ index.js
@@ -4,10 +4,32 @@ const app = express();
 
 const port = 7000;
 
+const users = [];
+
 app.set("view engine", "ejs");
+
+// for accessing the form data
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.render("index", { clubName: "Chelsea", clubName2: "Liverpool" });
+});
+
+app.post("/submit-form", (req, res) => {
+  console.log(req.body);
+
+  const { email, password } = req.body;
+
+  if (req.body === undefined) {
+    res.send("Something wrong with submitting the form");
+  } else {
+    users.push({ emailOfUser: email, passwordOfUser: password });
+    res.send("Form submitted!");
+  }
+});
+
+app.get("/users", (req, res) => {
+  res.json({ users });
 });
 
 app.listen(port, () => {
